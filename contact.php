bcf59433112afe14e7aac7391f5c19f2eea99071
Julian Purukan <julianpurukan1@gmail.com>
2019-12-12 14:28:23 +0700

    contact

---
+++ contact.php
@@ -0,0 +1,188 @@
+<?php
+  require_once("dbconfig.php");
+  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Negotiate - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style2.css">
+
+  
+  </head>
+  <body>
+	 <div class="bg-top navbar-light d-flex flex-column-reverse" >
+    	<div class="container py-3">
+    		<div class="row no-gutters d-flex align-items-center align-items-stretch" >
+    			<div class="col-md-4 d-flex align-items-center py-4" style="height:50px;">
+    				<!--<a class="navbar-brand" href="index.html">PT. Sinar Kokoh Persada <span>Consulting Agency</span></a>-->
+					<a class="navbar-brand" href="index.html"><img src="./images/logo-skp.png" alt="SKP Logo" width="80" height="80">PT. Sinar Kokoh Persada</a>
+    			</div>
+	    		
+		    </div>
+		  </div>
+		  
+    </div>
+	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container d-flex align-items-center">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+	     
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav mr-auto">
+	        	<li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
+	        	<li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+	        	<li class="nav-item"><a href="partners.html" class="nav-link">Partners</a></li>
+	        	<li class="nav-item"><a href="customers.html" class="nav-link">Customers</a></li>	        	
+				<li class="nav-item active"><a href="contact.php" class="nav-link">Contact</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Contact</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Contact <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+		
+		<section class="ftco-section contact-section">
+      <div class="container">
+        <div class="row d-flex mb-5 contact-info justify-content-center">
+        	<div class="col-md-10">
+        		<div class="row mb-5">
+		          <div class="col-md-4 text-center d-flex">
+		          	<div class="border w-100 p-4">
+			          	<div class="icon">
+			          		<span class="icon-map-o"></span>
+			          	</div>
+			            <p><span>Address:</span> Setiabudi Atrium Building 
+6th Floor Suite 610</br>Jl. HR Rasuna Said Kav 62 
+Jakarta Selatan 12920</p>
+			          </div>
+		          </div>
+		          <div class="col-md-4 text-center d-flex">
+		          	<div class="border w-100 p-4">
+			          	<div class="icon">
+			          		<span class="icon-tablet"></span>
+			          	</div>
+			            <p><span>Phone:</span> <a href="#"> +62 21 9328 174</a></p>
+						<p><span>Faximile:</span> <a href="#">  +62 21 9328 175</a></p>						
+			          </div>
+		          </div>
+		          <div class="col-md-4 text-center d-flex">
+		          	<div class="border w-100 p-4">
+			          	<div class="icon">
+			          		<span class="icon-envelope-o"></span>
+			          	</div>
+			            <p><span>Email:</span> <a href="mailto:info@skp.co.id">info@skp.co.id</a></p>
+						<p><span>Career:</span> <a href="mailto:contact@skp.co.id">contact@skp.co.id</a></p>						
+			          </div>
+		          </div>
+		        </div>
+          </div>
+        </div>
+        <div class="row block-9 justify-content-center mb-5">
+          <div class="col-md-10 mb-md-5">
+          <div class="contact-form">
+            <h2 class="text-center">If you got any questions <br>please do not hesitate to send us a message</h2>
+            <div id="sendmessage">Your message has been sent. Thank you!</div>
+              <div id="errormessage"></div>
+            <form action="insert.php" method="post" rolerole="form" class="contactForm">
+              <div class="form-group">
+                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" >
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject">
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <textarea name="message" data-rule="required" id="" cols="30" rows="7" class="form-control" placeholder="Message" data-msg="Please write something for us"></textarea>
+              </div>
+              <div class="form-group">
+                <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
+              </div>
+            </form>
+          </div>
+          </div>
+        </div>
+    </section>
+<!--
+    <section class="ftco-section ftco-no-pb ftco-no-pt">
+    	<div class="container-fluid px-0">
+    		<div class="row justify-content-center">
+        	<div class="col-md-12">
+        		<div id="map" class="bg-white"></div>
+        	</div>
+        </div>
+    	</div>
+    </section>
+		-->
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p>  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://skp.co.id" target="_blank">SKP Team</a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+  <script src="js/contactform.js"></script>
+  </body>
+</html>
